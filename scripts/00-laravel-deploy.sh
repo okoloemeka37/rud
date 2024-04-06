93e18e153679da4cf9e10db62c0ebbdb58d3da95
okoloemeka37 <okoloemeka37@gmail.com>
2024-04-06 05:54:14 +0100

    down

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -3,7 +3,8 @@ echo "Running composer"
 composer global require hirak/prestissimo
 composer install --no-dev --working-dir=/var/www/html
 
-
+echo "generating application key..."
+php artisan key:generate --show
 
 echo "Caching config..."
 php artisan config:cache
@@ -11,4 +12,5 @@ php artisan config:cache
 echo "Caching routes..."
 php artisan route:cache
 
-
+echo "Running migrations..."
+php artisan migrate --force
